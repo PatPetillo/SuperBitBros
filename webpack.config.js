dddedc244bf5ff30bbb8ad15c659e962bb7b7275
Bderiel <bderiel@gmail.com>
2017-11-09 16:50:25 -0500

    fix(webpack now works)

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,17 @@ module.exports = {
   entry: './client/index.js', // assumes your entry point is the index.js in the root of your project folder
   output: {
     path: __dirname,
-    filename: './src/bundle.js', // assumes your bundle.js will also be in the root of your project folder
+    filename: './public/bundle.js', // assumes your bundle.js will also be in the root of your project folder
   },
   devtool: 'source-map',
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.js?$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
-        options: { // if you aren't using 'babel-preset-es2015', then omit the 'es2015'
-        },
+        // options: { // if you aren't using 'babel-preset-es2015', then omit the 'es2015'
+        // },
       },
     ],
   },
