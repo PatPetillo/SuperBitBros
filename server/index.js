a11aa9d16ad06948e226b211072893918038b011
Bderiel <bderiel@gmail.com>
2017-11-09 16:08:35 -0500

    setup webpack and server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.listen(8080, () => {
+  console.log('listening on 8080 dawg');
+});
